fc5d79fc3ee5f2c16c681d15a7f97b9297188a5f
Miguel Tenente <codertenente@gmail.com>
2025-12-06 18:27:11 +0000

    Alteracao do porfolio em html css e index

--- index.js
+++ index.js
@@ -1,58 +1,34 @@
 //FUNCTIONS
 
-//Clear the #information section (MainScreen)
-function clearMainScreen () {
-    for (const element of infoList) {
-        element.style.display = 'none';
-    }
+//Menu Functions
+//Menu Button Listener to oppen the menu of the page
+function menuButtonListener () {
+    menuButton.onclick = function() {menuButtonManager()};
 }
 
-//Makes the letters of the elements from the #list thinner
-function thinList () {
-    for (const element of list) {
-        element.style.fontWeight = 'normal';
-    }
+function menuButtonManager () {
+    if (menu.style.display === 'none') openMenu();
+    else closeMenu();
 }
 
-//Manage what to show in the #information and respective list representetive
-function mainScreenManager (pos) {
-    clearMainScreen();
-    thinList();
-    if (pos < infoList.length - 1) {
-        list[pos].style.fontWeight = 'bolder';
-    }
-    infoList[pos].style.display = 'block';
+function openMenu () {
+    menu.style.display = 'block';
 }
 
-//Listener of what to show in the #information (MainScren)
-function mainScreenListener () {
-    for (let i = 0; i < infoList.length - 1; i++) {
-        list[i].onclick = function() {mainScreenManager(i)};
-    }
-
-    icon.onclick = function() {mainScreenManager(infoList.length - 1)};
+function closeMenu () {
+    menu.style.display = 'none';
 }
 
 
 
 //VARIABELS
+//Menu
+let menu = document.getElementById('menu');
+//Menu Button
+let menuButton = document.getElementById('menu_button');
 
-//Array of the elements that can appear on the #information section (MainScreen) 
-let infoList = [document.getElementById('about'),
-                document.getElementById('education'),
-                document.getElementById('experience'),
-                document.getElementById('software_projects'),
-                document.getElementById('automation_projects'),
-                document.getElementById('contact'),
-                document.getElementById('home')];
-
-//Having acess to the array of the li from #list                
-let list = document.getElementById('list').getElementsByTagName('a');
-
-//Icon from the webpage
-let icon = document.getElementById('header_icons').getElementsByTagName('a')[0];
 
 
 
 //ACTIONS FROM THE USER
-mainScreenListener();
+menuButtonListener();
